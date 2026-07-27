28f9c610e2fbad34974d34991a3b53df3d78be63
ADYAPRANA PRADHAN <120256356+Adyaprana@users.noreply.github.com>
2026-07-27 19:42:22 +0530

    Time: 0 ms (100%), Space: 23.1 MB (89.46%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,10 +1,18 @@
 class Solution(object):
     def rotate(self, nums, k):
-        arr = [0] * len(nums)
+        # nums[:] = nums[::-1]
         k = k % len(nums)
-        for i in range(len(nums)):
-            New_Index = (i + k) % len(nums)
-            arr[New_Index] = nums[i]
-        nums[:] = arr
-        # return nums
+        nums.reverse()
+        nums[0:k] = nums[0:k][::-1]
+        nums[k:] = nums[k:][::-1]
+      
+
+# class Solution(object):
+#     def rotate(self, nums, k):
+#         arr = [0] * len(nums)
+#         k = k % len(nums)
+#         for i in range(len(nums)):
+#             New_Index = (i + k) % len(nums)
+#             arr[New_Index] = nums[i]
+#         nums[:] = arr
         
