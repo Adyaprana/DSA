5f1b33feed6e7160e2d1318b17168ee1a0d7077f
ADYAPRANA PRADHAN <120256356+Adyaprana@users.noreply.github.com>
2026-07-29 23:55:36 +0530

    Time: 39 ms (97.58%), Space: 15.1 MB (21.98%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -26,26 +26,3 @@ class Solution(object):
         if min_subarr == total_sum:
             return max_subarr
         return max(max_subarr, total_sum - min_subarr)
-
-
-
-
-# class Solution(object):
-#     def maxSubarraySumCircular(self, nums):
-#         current_sum = nums[0]
-#         total_sum = nums[0]
-#         min_subarr = nums[0]
-#         max_subarr = nums[0]
-#         for i in range(1, len(nums)):
-#             total_sum += nums[i]
-#             if nums[i] < (current_sum + nums[i]):
-#                 current_sum = nums[i]
-#             else:
-#                 current_sum += nums[i]
-#             if current_sum < min_subarr:
-#                 min_subarr = current_sum
-
-#         total_sum -= (min_subarr)
-#         if min_subarr == total_sum:
-#             return 
-#         return total_sum 
