7b4c5f6ed783525e6bbe5c3a2a76e06f61e7af56
ADYAPRANA PRADHAN <120256356+Adyaprana@users.noreply.github.com>
2026-08-06 22:42:46 +0530

    Time: 15 ms (33.23%), Space: 20 MB (10.92%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,36 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
         freq = {}
-        for i in range(len(nums)):
-            freq[nums[i]] = freq.get(nums[i], 0) + 1
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
 
-        # sorted_desc = dict(sorted(freq.items(), key=lambda item: item[1], reverse=True))
-        # result = list(sorted_desc.keys())[:k]
-        
-        sorted_items = sorted(freq.items(), key=lambda item: item[1], reverse=True)
+        bucket = []
+        for i in range(len(nums) + 1):
+            bucket.append([])
+
+        for num, count in freq.items():
+            bucket[count].append(num)
+
         result = []
-        for num, count in sorted_items[:k]:
-            result.append(num)
-        return result
+        for i in range(len(bucket) - 1, 0, -1):
+            for num in bucket[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
+
+
+
+# class Solution(object):
+#     def topKFrequent(self, nums, k):
+
+#         freq = {}
+#         for i in range(len(nums)):
+#             freq[nums[i]] = freq.get(nums[i], 0) + 1
+
+#         sorted_items = sorted(freq.items(), key=lambda item: item[1], reverse=True)
+
+#         result = []
+#         for num, count in sorted_items[:k]:
+#             result.append(num)
+
+#         return result
