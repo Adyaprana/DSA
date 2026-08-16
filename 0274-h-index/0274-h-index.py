6b8c889903ca2b4877c2f536eb9bf70d439e02be
ADYAPRANA PRADHAN <120256356+Adyaprana@users.noreply.github.com>
2026-08-16 20:54:16 +0530

    Time: 0 ms (100%), Space: 12.4 MB (93.16%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,8 +1,26 @@
 class Solution(object):
-    def hIndex(self, citations):
-        citations.sort()
-        for i in range(0, len(citations)):
-            h = len(citations) - i
-            if citations[i] >= h:
+    def hIndex(self, citations):   
+        n = len(citations)
+        bucket = [0] * (n + 1)
+        for c in citations:
+            if c >= n:
+                bucket[n] += 1
+            else:
+                bucket[c] += 1
+        h = n
+        papers = 0
+        while h >= 0:
+            papers += bucket[h]
+            if papers >= h:
                 return h
+            h -= 1
         return 0
+
+# class Solution(object):
+#     def hIndex(self, citations):
+#         citations.sort()
+#         for i in range(0, len(citations)):
+#             h = len(citations) - i
+#             if citations[i] >= h:
+#                 return h
+#         return 0
