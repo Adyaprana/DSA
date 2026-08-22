113970298a650a39fc7a8b9c92e1295ec71e937f
ADYAPRANA PRADHAN <120256356+Adyaprana@users.noreply.github.com>
2026-08-22 11:58:00 +0530

    Time: 38 ms (42.2%), Space: 12.7 MB (20.96%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        seen = {}
+        
+        for i in magazine:            
+            if i not in seen:
+                seen[i] = seen.get(i, 0) + 1
+            elif i in seen:
+                seen[i] += 1
+        for i in ransomNote:
+            if i not in seen or seen[i] == 0:
+                return False
+            seen[i] -= 1
+        return True
