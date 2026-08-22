5ec0efa92dd5c0bfdb3991d3d31dc22bb3d404f9
ADYAPRANA PRADHAN <120256356+Adyaprana@users.noreply.github.com>
2026-08-22 12:07:10 +0530

    Time: 38 ms (42.2%), Space: 12.8 MB (20.96%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,13 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
         seen = {}
-        
         for i in magazine:            
-            if i not in seen:
-                seen[i] = seen.get(i, 0) + 1
-            elif i in seen:
-                seen[i] += 1
+            # if i not in seen:
+            #     seen[i] = seen.get(i, 0) + 1
+            # elif i in seen:
+            #     seen[i] += 1
+            seen[i] = seen.get(i, 0) + 1
+            
         for i in ransomNote:
             if i not in seen or seen[i] == 0:
                 return False
